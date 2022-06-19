3da149d74cf78a291592d9825f693cafeb948cd2
Ravi Shrestha <ravistha869@gmail.com>
2022-06-19 09:10:53 +0545

    fourth commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle, exitonclick
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,24 +8,22 @@ screen.bgcolor('black')
 screen.title('Snake Game')
 screen.tracer(0)
 
-starting_positions = [(0,0), (-20, 0), (-40, 0)]
 
-segments = []
 
-for position in starting_positions:
-    new_segment = Turtle('square')
-    new_segment.color('White')
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 
 
 game_is_on = True
 while game_is_on:
     screen.update() 
-    time.sleep(0.4)
-    for seg in segments:
-        seg.forward(20)
+    time.sleep(0.1)
     
-    segments 
-        
+    snake.move()
+
+screen.exitonclick()
