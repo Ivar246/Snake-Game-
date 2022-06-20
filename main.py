0b128d30bd98ccdbb021f5b9b287790268e2e631
Ravi Shrestha <ravistha869@gmail.com>
2022-06-20 22:24:38 +0545

    fifth commit

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-score = Scoreboard()
+scoreboard = Scoreboard()
 
 
 screen.listen()
--- score_board.py
+++ score_board.py
@@ -3,6 +3,10 @@ import turtle
 
 class Scoreboard(turtle.Turtle):
     def __init__(self):
+        super().__init__()
         self.score = 0
         self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
         self.write(f'score: {self.score}', move=False, align='center', font=('arial', 25, 'normal'))
