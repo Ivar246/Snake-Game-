e71596e3d8e7c06b949f288716a100ef19095db6
Ravi Shrestha <ravistha869@gmail.com>
2022-06-20 22:18:58 +0545

    fifth commit

--- score_board.py
+++ score_board.py
@@ -2,6 +2,7 @@ import turtle
 
 
 class Scoreboard(turtle.Turtle):
-    def __init__(self, score):
-        self.clear()
+    def __init__(self):
+        self.score = 0
+        self.color("white")
         self.write(f'score: {self.score}', move=False, align='center', font=('arial', 25, 'normal'))
