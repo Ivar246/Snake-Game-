c71e94938a917c6331218107d4779529ddec07a7
Ravi Shrestha <ravistha869@gmail.com>
2022-06-21 18:41:57 +0545

    sixth commit

--- food.py
+++ food.py
@@ -14,8 +14,8 @@ class Food(Turtle):
         
         
     def refresh(self):
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
+        random_x = random.randint(-270, 270)
+        random_y = random.randint(-270, 270)
         self.goto(random_x, random_y)
         
         
--- main.py
+++ main.py
@@ -34,11 +34,19 @@ while game_is_on:
     # collision with food
     if snake.head.distance(food) < 15: 
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
     
+    # detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
         
-
+    # detect collision with tail 
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+            
+            
 screen.exitonclick()
